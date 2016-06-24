73a4863b061fbc4e1983e9b0ec3753decb489a5f
Chisaka <a1502020@sendai-nct.jp>
2016-06-24 23:52:28 +0900

    迷路の大きさを実行中に変更できるようにした

--- Automaze/Main.cpp
+++ Automaze/Main.cpp
@@ -12,6 +12,13 @@ void Main()
 	GUI gui(GUIStyle::Default);
 	gui.setTitle(L"迷路自動生成オプション");
 
+	gui.add(GUIText::Create(L"幅"));
+	gui.add(L"w", GUISlider::Create(7, 101, w));
+	gui.add(GUINewLine::Create());
+	gui.add(GUIText::Create(L"高さ"));
+	gui.add(L"h", GUISlider::Create(7, 101, h));
+	gui.add(GUINewLine::Create());
+
 	gui.add(L"algo", GUIRadioButton::Create({ L"棒倒し法", L"道延ばし法", L"壁延ばし法" }, 0, true));
 	std::vector<std::shared_ptr<MazeFactory>> factories = {
 		std::make_shared<Boutaoshi>(),
@@ -28,7 +35,9 @@ void Main()
 		Graphics3D::FreeCamera(1.0);
 
 		// 迷路生成
-		if (gui.radioButton(L"algo").hasChanged) {
+		if (gui.slider(L"w").hasChanged || gui.slider(L"h").hasChanged || gui.radioButton(L"algo").hasChanged) {
+			w = static_cast<int>(gui.slider(L"w").value) / 2 * 2 + 1;
+			h = static_cast<int>(gui.slider(L"h").value) / 2 * 2 + 1;
 			maze = factories[gui.radioButton(L"algo").checkedItem.value_or(0)]->create(w, h, 1, 1, w - 2, h - 2);
 		}
 
